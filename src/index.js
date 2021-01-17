08d797f0732343fdfffc08d7bd06a553fc898a43
Saki Hayashi <saki.hayashi@codeimmersive.com>
2021-01-16 20:50:07 -0500

    pre-render

--- src/index.js
+++ src/index.js
@@ -12,15 +12,25 @@ import thunk from 'redux-thunk'
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>, 
+  rootElement);
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
