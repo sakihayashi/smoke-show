438a1e229711c379776b400ccee66c030c795b3f
Saki Hayashi <saki.hayashi@codeimmersive.com>
2021-01-19 16:44:22 -0500

    test

--- src/index.js
+++ src/index.js
@@ -14,23 +14,31 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const rootElement = document.getElementById("root");
 
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(
-    <React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>, 
-  rootElement);
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-  rootElement);
-}
+document.getElementById('root'));
+
+// if (rootElement.hasChildNodes()) {
+//   ReactDOM.hydrate(
+//     <React.StrictMode>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </React.StrictMode>, 
+//   rootElement);
+// } else {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </React.StrictMode>,
+//   rootElement);
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
