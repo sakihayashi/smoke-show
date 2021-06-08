f411f450a96747aae40a177e2f65c64cad970b65
Saki Hayashi <sh3467@nyu.edu>
2021-06-07 20:44:42 -0400

    video

--- src/components/Admin/Instagram.js
+++ src/components/Admin/Instagram.js
@@ -12,7 +12,7 @@ const Instagram = () =>{
     useEffect(() =>{
         // axios.get(`https://graph.instagram.com/me/media?fields=id,caption,permalink,media_url&access_token=${process.env.REACT_APP_INSTA}`)
         // IGQVJVLTJSRmRhemp3MTBqekJZAeFZAfYzFZANjJreEhCc1lVY05FaWd4T0tfN1NLVWp2WG52MG5ETTByR3NSVFo3M01PWk4tdFpzYzN2Ql9XMVRmelRmVEhwbFFwSDgzdzVOdDU5TkJB
-        axios.get(`https://graph.instagram.com/me/media?fields=id,caption,permalink,media_url&access_token=IGQVJVLTJSRmRhemp3MTBqekJZAeFZAfYzFZANjJreEhCc1lVY05FaWd4T0tfN1NLVWp2WG52MG5ETTByR3NSVFo3M01PWk4tdFpzYzN2Ql9XMVRmelRmVEhwbFFwSDgzdzVOdDU5TkJB`)
+        axios.get(`https://graph.instagram.com/me/media?fields=id,caption,permalink,media_url,media_type&access_token=IGQVJVLTJSRmRhemp3MTBqekJZAeFZAfYzFZANjJreEhCc1lVY05FaWd4T0tfN1NLVWp2WG52MG5ETTByR3NSVFo3M01PWk4tdFpzYzN2Ql9XMVRmelRmVEhwbFFwSDgzdzVOdDU5TkJB`)
       .then(res => {
         setInstaData(res.data.data)
       })
@@ -29,12 +29,23 @@ const Instagram = () =>{
                     {instaData[0] && 
                         instaData.map(data =>{
                             const instaLink = data.permalink.replace('/', '')
-                            console.log('data.media_url', data.media_url)
+                            console.log('media', data.media_type)
                             // const srcLink = data.media_url.replace('/', '')
                             return (
                                 <Col md={4}>
-                                    <Card style={{ width: '100%' }} className="square">
-                                        <Card.Img variant="top" src={data.media_url} />
+                                    <Card style={{ width: '100%' }} >
+                                        <div className="square">
+                                            { data.media_type === 'VIDEO' ? 
+                                            <video className="insta-video" controls>
+                                            <source src={data.media_url}
+                                            type="video/mp4"/>
+                                            </video>
+                                            :
+                                            <Card.Img variant="top" src={data.media_url} />
+                                            }
+                                        
+                                        </div>
+                                        
                                         <Card.Body>
                                             {/* <Card.Title>Card Title</Card.Title> */}
                                             <Card.Text>
